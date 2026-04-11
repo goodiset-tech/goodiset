04454143ac7d3c371badf63f0c6bfa99a462b053
carloslennix <carloslennix38@gmail.com>
2026-04-11 01:25:05 -0400

    migrate

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        \Artisan::call('migrate', ['--force' => true]);
+        \Artisan::call('db:seed', ['--force' => true]);
     }
 }
