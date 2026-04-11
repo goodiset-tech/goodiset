567c79556cab3cc6e0f1f8a09af7586dade68736
carloslennix <carloslennix38@gmail.com>
2026-04-11 01:46:01 -0400

    remove admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call([
 
-            AdminSeeder::class,
+           # AdminSeeder::class,
             SettingSeeder::class,
             UserSeeder::class,
             PermissionSeeder::class,
