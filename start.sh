f7940743caae1eecd3357385d8a1da22e91b6dc0
carloslennix <carloslennix38@gmail.com>
2026-04-12 16:49:04 -0400

    # fix

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ set -e
 cd /var/www/html
 
 # Ensures `sessions` table exists when SESSION_DRIVER=database (recommended on Railway).
-php artisan migrate --force --no-interaction
+# php artisan migrate --force --no-interaction
 
 # Start PHP-FPM in the background
 php-fpm -D
