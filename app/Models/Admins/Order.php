abfe673411cf3279b2971ff4a4fe47266af4322f
carloslennix <carloslennix38@gmail.com>
2026-04-12 13:39:44 -0400

    #order

--- app/Models/Admins/Order.php
+++ app/Models/Admins/Order.php
@@ -10,7 +10,45 @@ class Order extends Model
 {
     use HasFactory, SoftDeletes;
     protected $table = 'orders';
-
+    protected $fillable = [
+        'product_detail',
+        'order_no',
+        'customer_name',
+        'email',
+        'uid',
+        'amount',
+        'city',
+        'phone',
+        'country',
+        'address',
+        'shipping_company',
+        'track_url',
+        'track_no',
+        'note',
+        'status',
+        'dstatus',
+        'state',
+        'postal_code',
+        'unit',
+        'unit_number',
+        'company_name',
+        'payment_method',
+        'payment_status',
+        'transaction_id',
+        'order_note',
+        'package_id',
+        'package_size_id',
+        'package_name',
+        'package_detail',
+        'package_qty',
+        'payment_ref',
+        'shipping_fee',
+        'vat',
+        'discount',
+        'coupon_code',
+        'session_id',
+        'driver_token',
+    ];
     public function getEncryptedOrderNoAttribute()
     {
         return \Illuminate\Support\Facades\Crypt::encryptString($this->order_no);
