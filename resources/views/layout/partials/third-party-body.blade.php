109f633b5cb3b1018ed256635d54e0c6cd39c5c9
carloslennix <carloslennix38@gmail.com>
2026-04-13 20:26:52 -0400

    # facebook pixel

--- resources/views/layout/partials/third-party-body.blade.php
+++ resources/views/layout/partials/third-party-body.blade.php
@@ -2,3 +2,8 @@
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KD4877QQ" height="0" width="0"
             style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
+        <!-- Facebook Pixel Code -->    
+         <noscript><img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=4013696122240068&ev=PageView&noscript=1"
+        /></noscript>
+        <!-- End Facebook Pixel Code -->
